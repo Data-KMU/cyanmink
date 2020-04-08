b37d4f060f4ba4683898376ec5c048dc7785b644
Felix Mayr <felixmayr99@gmail.com>
2020-04-08 23:02:02 +0200

    Different Layers show on zoom, Added Test GeoJSON

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,5 +1,9 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import ReactMapboxGl from "react-mapbox-gl";
+
+import Layer from "./Layer";
+
+import testjson from "../json/test.json";
 
 const Mapbox = ReactMapboxGl({
   accessToken:
@@ -7,16 +11,54 @@ const Mapbox = ReactMapboxGl({
   dragRotate: false,
 });
 
-const Map = () => (
-  <Mapbox
-    center={[12.1692, 47.5827]}
-    zoom={[13]}
-    style="mapbox://styles/felixmayr/ck4h2kv441spq1co7x7hh17g0"
-    containerStyle={{
-      height: "100vh",
-      width: "100vw",
-    }}
-  />
-);
+const Map = () => {
+  const [zoom, setZoom] = useState(7);
+  const [position, setPosition] = useState([12.1692, 47.5827]);
+
+  console.log("Zoom:" + zoom);
+
+  return (
+    <>
+      <Mapbox
+        center={position}
+        zoom={[zoom]}
+        style="mapbox://styles/felixmayr/ck4h2kv441spq1co7x7hh17g0"
+        containerStyle={{
+          height: "100vh",
+          width: "100vw",
+        }}
+        onZoomEnd={(_) => {
+          setZoom(_.getZoom());
+        }}
+      >
+        <Area zoom={zoom} />
+      </Mapbox>
+    </>
+  );
+};
+
+const Area = ({ zoom }) => {
+  const regions = [];
+  const provinces = [];
+  const cities = [];
+
+  for (let i = 0; i < testjson.features.length; i++) {
+    if (testjson.features[i].properties.priority === 0) {
+      regions.push(testjson.features[i]);
+    } else if (testjson.features[i].properties.priority === 10) {
+      provinces.push(testjson.features[i]);
+    } else if (testjson.features[i].properties.priority === 100) {
+      cities.push(testjson.features[i]);
+    }
+  }
+
+  if (zoom < 7.5) {
+    return <Layer data={regions} color={"red"} />;
+  } else if (zoom < 9) {
+    return <Layer data={provinces} color={"green"} />;
+  } else if (zoom > 9) {
+    return <Layer data={cities} color={"blue"} />;
+  }
+};
 
 export default Map;
